6a70c10e7ea5194d94dcfc55820342a2bb52ec24
its-justus <ianjohnson9042@gmail.com>
2020-07-06 16:28:19 -0500

    updated database tables

--- database.sql
+++ database.sql
@@ -1,11 +1,93 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE account (
-		id 								SERIAL PRIMARY KEY,
-		username 					VARCHAR (80) UNIQUE NOT NULL,
-		password_hashed 	VARCHAR (1000) NOT NULL,
-		created_at				TIMESTAMP NOT NULL DEFAULT NOW()
+-- built with dbdesigner.net
+CREATE TABLE "account" (
+	"id" serial NOT NULL,
+	"username" varchar(80) NOT NULL UNIQUE,
+	"password_hashed" 	VARCHAR (1000) NOT NULL,
+	"created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+	CONSTRAINT "account_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "profile" (
+	"account_id" integer NOT NULL,
+	"email" varchar(320) NOT NULL UNIQUE,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	CONSTRAINT "profile_pk" PRIMARY KEY ("account_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "topic" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT "topic_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "convo" (
+	"id" serial NOT NULL,
+	"topic_id" integer NOT NULL,
+	"created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+	CONSTRAINT "convo_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "convo_member" (
+	"account_id" integer NOT NULL,
+	"convo_id" integer NOT NULL,
+	CONSTRAINT "convo_member_pk" PRIMARY KEY ("account_id","convo_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "favorite_topic" (
+	"account_id" integer NOT NULL,
+	"topic_id" integer NOT NULL,
+	CONSTRAINT "favorite_topic_pk" PRIMARY KEY ("account_id","topic_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "message" (
+	"id" serial NOT NULL,
+	"author_id" integer NOT NULL,
+	"convo_id" integer NOT NULL,
+	"text" VARCHAR(4000) NOT NULL,
+	CONSTRAINT "message_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "profile" ADD CONSTRAINT "profile_fk0" FOREIGN KEY ("account_id") REFERENCES "account"("id");
+
+
+ALTER TABLE "convo" ADD CONSTRAINT "convo_fk0" FOREIGN KEY ("topic_id") REFERENCES "topic"("id");
+
+ALTER TABLE "convo_member" ADD CONSTRAINT "convo_member_fk0" FOREIGN KEY ("account_id") REFERENCES "account"("id");
+ALTER TABLE "convo_member" ADD CONSTRAINT "convo_member_fk1" FOREIGN KEY ("convo_id") REFERENCES "convo"("id");
+
+ALTER TABLE "favorite_topic" ADD CONSTRAINT "favorite_topic_fk0" FOREIGN KEY ("account_id") REFERENCES "account"("id");
+ALTER TABLE "favorite_topic" ADD CONSTRAINT "favorite_topic_fk1" FOREIGN KEY ("topic_id") REFERENCES "topic"("id");
+
+ALTER TABLE "message" ADD CONSTRAINT "message_fk0" FOREIGN KEY ("author_id") REFERENCES "account"("id");
+ALTER TABLE "message" ADD CONSTRAINT "message_fk1" FOREIGN KEY ("convo_id") REFERENCES "convo"("id");
