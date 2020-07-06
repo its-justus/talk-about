c9c0bb4e1b92fbf15b97148c137d6ad3e18a7698
its-justus <ianjohnson9042@gmail.com>
2020-07-06 15:53:19 -0500

    build for prod v0.1

---
+++ build/precache-manifest.9219be2e867544eb7e111ef2d69583c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92990c2ff53e48d5803355de7d59aeb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6da8e11e9bbe985ed6",
+    "url": "/static/css/main.b7f141da.chunk.css"
+  },
+  {
+    "revision": "8544e882b6c33ac8ba40",
+    "url": "/static/js/2.f8357a53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f8357a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6da8e11e9bbe985ed6",
+    "url": "/static/js/main.717a1bd8.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
