5862baf043c7afe2fad1cd8da74697579a861208
its-justus <ianjohnson9042@gmail.com>
2020-07-06 17:45:30 -0500

    added basic mui grid formatting to hide sidebars on xs size

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Grid, Box, Hidden } from "@material-ui/core";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        Chatroom
+        <Grid container justify="center">
+				<Hidden xsDown>
+          <Grid item name="left-section" xs={0}>
+					<Box width={200}>left box</Box>
+          </Grid>
+					</Hidden>
+          <Grid item name="center-section" xs>
+            center
+          </Grid>
+					<Hidden xsDown>
+          <Grid item name="right-section" xs={0}>
+					<Box width={200}>right box</Box>
+          </Grid>
+					</Hidden>
+        </Grid>
       </div>
     );
   }
