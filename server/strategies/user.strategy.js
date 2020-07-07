13ff8e027c73cd17b7de2bb73a010abf6c3bdc6a
its-justus <ianjohnson9042@gmail.com>
2020-07-07 16:53:08 -0500

    fixed authentication bug

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -36,7 +36,7 @@ passport.use('local', new LocalStrategy((username, password, done) => {
     pool.query('SELECT * FROM account WHERE username = $1', [username])
       .then((result) => {
         const user = result && result.rows && result.rows[0];
-        if (user && encryptLib.comparePassword(password, user.password_hashed)) {
+        if (user && encryptLib.comparePassword(password, user.password)) {
           // All good! Passwords match!
           // done takes an error (null in this case) and a user
           done(null, user);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,7 +21,7 @@ import './App.css';
 
 class App extends Component {
   componentDidMount () {
-    this.props.dispatch({type: 'FETCH_USER'})
+		this.props.dispatch({type: 'FETCH_USER'})
   }
 
   render() {
