c61614fdc8c985fef60e243cb1815c6de1e96f2c
its-justus <ianjohnson9042@gmail.com>
2020-07-11 14:34:13 -0500

    forgot to add member socket woops

---
+++ server/sockets/member.socket.js
@@ -0,0 +1,22 @@
+const pool = require("../modules/pool");
+
+function getMembers(payload, socket, io) {
+	console.log("getMembers");
+	const queryText = `SELECT account.id, account.username FROM room_member
+		JOIN account ON account.id = room_member.account_id
+		WHERE room_id = $1
+		ORDER BY account.username ASC;`;
+  const queryValues = [1];
+  pool
+    .query(queryText, queryValues)
+    .then((result) => {
+      socket.emit("member.refresh", result.rows);
+    })
+    .catch((error) => {
+      console.log("query error", error);
+    });
+}
+
+module.exports = {
+  getMembers: getMembers,
+};
