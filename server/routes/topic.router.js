53552547c83daa77f67f218cc6a8e27d527c6f7d
its-justus <ianjohnson9042@gmail.com>
2020-07-12 12:19:24 -0500

    added topic entry field

--- server/routes/topic.router.js
+++ server/routes/topic.router.js
@@ -6,7 +6,17 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  console.log("ROUTE: GET /api/topic/");
+	console.log("ROUTE: GET /api/topic/");
+	
+	const queryText = `SELECT * FROM topic;`
+	const queryValues = [];
+	pool.query(queryText, queryValues)
+		.then((result) => {
+			res.status(200).send(result.rows);
+		})
+		.catch((error) => {
+			res.sendStatus(500);
+		})
 });
 
 /**
--- src/components/TopicExplorer/TopicExplorer.js
+++ src/components/TopicExplorer/TopicExplorer.js
@@ -3,11 +3,37 @@ import { connect } from "react-redux";
 import { Grid, Box, Hidden } from "@material-ui/core";
 
 class TopicExplorer extends React.Component {
+  state = {
+    topicInput: "",
+	};
+	
+	joinTopic = (event) => {
+		event.preventDefault();
+		this.props.dispatch({type: "JOIN_TOPIC", payload: this.state.topicInput});
+	}
+
   render() {
     return (
       <div>
+        Enter topic:
+				<form onSubmit={this.joinTopic}>
+        <input
+					required
+					minLength={3}
+          type="text"
+          value={this.state.topicInput}
+          onChange={(event) =>
+            this.setState({ topicInput: event.target.value })
+          }
+        />
+				</form>
         Topics:
-				<button type="button" onClick={() => this.props.dispatch({type: "REFRESH_TOPICS"})}>Refresh</button>
+        <button
+          type="button"
+          onClick={() => this.props.dispatch({ type: "REFRESH_TOPICS" })}
+        >
+          Refresh
+        </button>
         {this.props.topics?.map((cur, i) => (
           <p key={`topic-${i}`}>{cur.name}</p>
         ))}
