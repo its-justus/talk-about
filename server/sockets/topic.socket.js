90ce9aed7fd79d0dcfc164106d4297586b0dbdf0
its-justus <ianjohnson9042@gmail.com>
2020-07-14 15:10:20 -0500

    added makeRoom function

--- server/sockets/topic.socket.js
+++ server/sockets/topic.socket.js
@@ -154,8 +154,39 @@ async function joinTopic(payload, socket, io) {
  * @returns {roomObj} a room object the user will then join
  */
 async function getRoom(topic, uid) {
+	const MAX_MEMBER_COUNT = 7;
 	try {
 		console.log("topic.getRoom");
+
+		// define our query
+		// Our query selects a single room that has a topic matching our topic
+		// param, fewer members that MAX_MEMBER_COUNT, and the user is not
+		// already a member of. If there are no such rooms our result.rows is an
+		// empty array. It also selects the oldest room, so older rooms get priority
+		// when filling empty spots.
+		const query = {
+			text: `SELECT room.*, count(room_member)::INTEGER AS member_count FROM room
+				JOIN topic ON topic.id = room.topic_id
+				JOIN room_member ON room_member.room_id = room.id
+				WHERE topic.name = $1 AND
+				room.id NOT IN
+					(SELECT room_id FROM room_member WHERE account_id = $2)
+				GROUP BY room.id
+				HAVING count(room_member) < $3
+				ORDER BY room.created_at ASC
+				LIMIT 1;`,
+			values: [topic, uid, MAX_MEMBER_COUNT],
+		};
+
+		// submit our query and assign the first row to room
+		let room = await pool.query(query.text, query.values);
+		room = room.rows[0];
+		// if we didn't get a room back, make one
+		if(!room) {
+			room = await makeRoom(topic);
+		}
+		console.log("room:", room);
+
 	} catch (error) {
 		console.log("topic.getRoom error", error);
 	}
@@ -213,71 +244,83 @@ async function addUserToRoom(userID, roomID) {
   }
 }
 
-async function makeRoomForUser(topic, userID) {
-  try {
-    console.log("makeRoomForUser", { topic, userID });
-    const query = {};
+/**
+ * makeRoom adds a room to the database with the specified topic. 
+ * 
+ * @param {string} topic the topic the room will start with
+ * @returns {roomObj} a room object containing data about the room
+ */
+async function makeRoom(topic) {
+	try {
+		console.log("makeRoom", topic);
+	} catch (error) {
+		console.log("makeRoom error:", error);
+	}
 
-    // begin our transaction
-    await pool.query("BEGIN");
+  // try {
+  //   console.log("makeRoomForUser", { topic, userID });
+  //   const query = {};
 
-    // first get the topic id if it exists for our topic
-    query.text = `SELECT id FROM topic WHERE name = $1;`;
-    query.values = [topic];
-    query.result = await pool.query(query.text, query.values);
+  //   // begin our transaction
+  //   await pool.query("BEGIN");
 
-    console.log("select topics successfull", query.result.rows);
+  //   // first get the topic id if it exists for our topic
+  //   query.text = `SELECT id FROM topic WHERE name = $1;`;
+  //   query.values = [topic];
+  //   query.result = await pool.query(query.text, query.values);
 
-    // if we got a topic id back
-    if (query.result.rowCount > 0) {
-      // make a new room for that topic
-      const topicID = query.result.rows[0].id;
-      query.text = `INSERT INTO room (topic_id)
-				VALUES ($1)
-				RETURNING *;`;
-      query.values = [topicID];
-      query.result = await pool.query(query.text, query.values);
+  //   console.log("select topics successfull", query.result.rows);
 
-      console.log("insert room successfull", query.result.rows[0]);
+  //   // if we got a topic id back
+  //   if (query.result.rowCount > 0) {
+  //     // make a new room for that topic
+  //     const topicID = query.result.rows[0].id;
+  //     query.text = `INSERT INTO room (topic_id)
+	// 			VALUES ($1)
+	// 			RETURNING *;`;
+  //     query.values = [topicID];
+  //     query.result = await pool.query(query.text, query.values);
 
-			console.log("insert row: ", query.result.rows);
-			await addUserToRoom(userID, query.result.rows[0].id)
-			pool.query("COMMIT");
-			return query.result.rows[0].id;
-      // then add the user to that room
-      // await addUserToRoom(userID, topicID);
-    } else {
-      // otherwise first make a new topic
-      query.text = `INSERT INTO topic (name)
-				VALUES ($1)
-				RETURNING *;`;
-      query.values = [topic];
-      query.result = await pool.query(query.text, query.values);
+  //     console.log("insert room successfull", query.result.rows[0]);
 
-      console.log("insert topic successfull", query.result.rows[0]);
+	// 		console.log("insert row: ", query.result.rows);
+	// 		await addUserToRoom(userID, query.result.rows[0].id)
+	// 		pool.query("COMMIT");
+	// 		return query.result.rows[0].id;
+  //     // then add the user to that room
+  //     // await addUserToRoom(userID, topicID);
+  //   } else {
+  //     // otherwise first make a new topic
+  //     query.text = `INSERT INTO topic (name)
+	// 			VALUES ($1)
+	// 			RETURNING *;`;
+  //     query.values = [topic];
+  //     query.result = await pool.query(query.text, query.values);
 
-      // then make a room for that topic
-      query.text = `INSERT INTO room (topic_id)
-				VALUES ($1)
-				RETURNING *;`;
-      query.values = [query.result.rows[0].id];
-      query.result = await pool.query(query.text, query.values);
-      console.log("insert room successfull", query.result.rows[0]);
+  //     console.log("insert topic successfull", query.result.rows[0]);
 
-      // then add our user to the room
-      await addUserToRoom(userID, query.result.rows[0].id);
+  //     // then make a room for that topic
+  //     query.text = `INSERT INTO room (topic_id)
+	// 			VALUES ($1)
+	// 			RETURNING *;`;
+  //     query.values = [query.result.rows[0].id];
+  //     query.result = await pool.query(query.text, query.values);
+  //     console.log("insert room successfull", query.result.rows[0]);
 
-      // commit our changes
-      await pool.query("COMMIT");
+  //     // then add our user to the room
+  //     await addUserToRoom(userID, query.result.rows[0].id);
 
-      // finally return our new room id
-      return query.result.rows[0].id;
-    }
-  } catch (error) {
-    // if we get an error, rollback our transaction
-    pool.query("ROLLBACK");
-    throw error;
-  }
+  //     // commit our changes
+  //     await pool.query("COMMIT");
+
+  //     // finally return our new room id
+  //     return query.result.rows[0].id;
+  //   }
+  // } catch (error) {
+  //   // if we get an error, rollback our transaction
+  //   pool.query("ROLLBACK");
+  //   throw error;
+  // }
 }
 
 module.exports = {
