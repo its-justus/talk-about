dea1088998ea3e648c6504c2c15692e94ec17f4c
its-justus <ianjohnson9042@gmail.com>
2020-07-13 21:33:50 -0500

    added getRoomMembers function

--- server/sockets/session.socket.js
+++ server/sockets/session.socket.js
@@ -14,6 +14,7 @@ async function start(data, socket, io) {
 	// pull our user id from our passport session
 	const { user } = socket.request.session.passport;
 	console.log('session.start user:', user);
+	const startTime = Date.now()
 
 	// OPTIMIZATION: combine these functions into a single pg query.
 	// I'm really only splitting these functions apart for legibility 
@@ -22,6 +23,12 @@ async function start(data, socket, io) {
 	const rooms = await getUserRooms(user);
 	// query db for topics of user's rooms
 	const topics = await getUserTopics(user);
+
+	// loop through each room in rooms
+	for(let room of rooms){
+		room.members = await getRoomMembers(room.id);
+	}
+	console.log('run time (ms):', Date.now() - startTime);
 }
 
 /**
@@ -64,6 +71,28 @@ async function getUserTopics(userID) {
 			JOIN room_member ON room.id = room_member.room_id
 			WHERE room_member.account_id = $1;`,
 		values: [userID],
+	};
+	// submit query to pool
+	query.result = await pool.query(query.text, query.values);
+	console.log("query result:",query.result.rows);
+	return query.result.rows;
+}
+
+/**
+ * getRoomMembers queries the database for the room members of a specific room
+ * 
+ * @param {integer} roomID the id of the room to get the members for
+ * @returns {array} an array of account objects
+ */
+async function getRoomMembers(roomID) {
+	console.log("getRoomMembers:", roomID);
+
+	// define our query
+	const query = {
+		text: `SELECT account.id, account.username FROM account
+			JOIN room_member ON account.id = room_member.account_id
+			WHERE room_member.room_id = $1;`,
+		values: [roomID],
 	};
 	// submit query to pool
 	query.result = await pool.query(query.text, query.values);
