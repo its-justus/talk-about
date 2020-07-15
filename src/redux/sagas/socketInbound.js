840117704b108cc8eea8b5f4845e5a3a9ca52206
its-justus <ianjohnson9042@gmail.com>
2020-07-15 14:29:41 -0500

    cleaned up socketInbound, removed deprecated event handlers

--- src/redux/sagas/socketInbound.js
+++ src/redux/sagas/socketInbound.js
@@ -4,13 +4,7 @@ import { call, take, put } from "redux-saga/effects";
 // messages channel returns
 function socketChannel(socket) {
   return new eventChannel((dispatch) => {
-    // message handlers
-    // message refresh resets all messages
-    socket.on("message.refresh", (data) => {
-      console.log("Messages refreshed:", data);
-      dispatch({ type: "SET_MESSAGES", payload: data.reverse() });
-    });
-
+    // Message Events
     // message receive adds a new message to the stream
     socket.on("message.receive", (data) => {
       console.log("Message received:", data);
@@ -30,27 +24,13 @@ function socketChannel(socket) {
     });
 
     // Member events
-
-    // member refresh updates the member list
-    socket.on("member.refresh", (data) => {
-      console.log("Members refreshed", data);
-      dispatch({ type: "SET_MEMBERS", payload: data });
-    });
-
     // member.new is sent when a new member joins the room
     socket.on("member.new", (data) => {
       console.log("New member joined", data);
-      dispatch({ type: "ADD_MEMBER", payload: data });
+      dispatch({ type: "ADD_MEMBER_TO_ROOM", payload: data });
     });
 
     // Room Events
-
-    // room refresh
-    socket.on("room.refresh", (data) => {
-      console.log("Rooms refreshed");
-      dispatch({ type: "SET_ROOMS", payload: data });
-    });
-
     // room joined
     socket.on("room.joined", (data) => {
       console.log("Joined room", data);
@@ -80,11 +60,13 @@ function socketChannel(socket) {
       });
     });
 
+		// Topic Events
     // receive popular topics
     socket.on("topic.popularTopics", (data) => {
       console.log("Popular topics:", data);
     });
 
+		// Session Events
     // server letting us know our session is all ready
     socket.on("session.ready", (data) => {
       console.log("Session ready, hooray!");
