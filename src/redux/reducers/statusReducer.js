586d7836837fb4c2ae1f07e68fe26b7ded6a6f6e
its-justus <ianjohnson9042@gmail.com>
2020-07-14 22:54:03 -0500

    added status reducer

---
+++ src/redux/reducers/statusReducer.js
@@ -0,0 +1,12 @@
+const statusReducer = (state = "STARTING", action) => {
+	let newState = state.slice(0);
+	switch (action.type) {
+		case "SET_STATUS":
+			newState = action.payload.slice(0);
+			return newState;
+		default:
+			return newState;
+	}
+}
+
+export default statusReducer;
