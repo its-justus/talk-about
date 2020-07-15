a0964385a7de311fbbfbcb5823b2bff4ec8254e0
its-justus <ianjohnson9042@gmail.com>
2020-07-15 11:32:55 -0500

    updated roomsReducer to accept data for individual rooms

--- src/redux/reducers/roomsReducer.js
+++ src/redux/reducers/roomsReducer.js
@@ -1,11 +1,26 @@
+/**
+ * the rooms reducer stores data for each room the user is an active member of.
+ * 
+ * A roomObject is currently defined as:
+ * {
+ * 	id: integer, 
+ * 	created_at: ISO8601 date, 
+ * 	topic_id: integer
+ * }
+*/
 const roomsReducer = (state = [], action) => {
 	let newState = [...state];
 	switch (action.type) {
 		case 'SET_ROOMS':
-			// set rooms expects an array of room objects
-			newState = action.payload;
-			console.log("SET ROOMS: ", newState);
+			// action.payload = [{roomObject1}, {roomObject2},...]
+			newState = [...action.payload];
 			return newState;
+		case 'ADD_ROOM':
+			// action.payload = {roomObject} 
+			newState = [...newState, action.payload];
+			return newState;
+		case 'RESET_ROOMS':
+			return [];
 		default:
 			return state;
 	}
--- src/redux/reducers/topicsReducer.js
+++ src/redux/reducers/topicsReducer.js
@@ -1,23 +1,23 @@
 /**
  * The topics reducer is currently just a way to get a topics proper name
- * based on a topic id that might live in another object. 
+ * based on a topic id that might live in another object.
  * Referencing a topic should be done by mapping the reducer to props and then
- * referencing this.props.topics[topicID]. This will give the name of the topic. 
- * 
+ * referencing this.props.topics[topicID]. This will give the name of the topic.
+ *
  * Expected action data structures are included in each case.
  */
 const topicsReducer = (state = {}, action) => {
-	let newState = {...state};
-	switch (action.type) {
-    case 'SET_TOPICS':
-			// action.payload = {topic.id1: topic.name1, topic.id2: topic.name2...}
-			return {...action.payload};
-		case 'ADD_TOPIC':
-			// action.payload = {id: topic.id, name: topic.name}
-			newState[action.payload.id] = action.payload.name;
-			return newState;
-    case 'RESET_TOPICS':
-			// action.payload = N/A
+  let newState = { ...state };
+  switch (action.type) {
+    case "SET_TOPICS":
+      // action.payload = {topic.id1: topic.name1, topic.id2: topic.name2...}
+      return { ...action.payload };
+    case "ADD_TOPIC":
+      // action.payload = {id: topic.id, name: topic.name}
+      newState[action.payload.id] = action.payload.name;
+      return newState;
+    case "RESET_TOPICS":
+      // action.payload = N/A
       return {};
     default:
       return state;
