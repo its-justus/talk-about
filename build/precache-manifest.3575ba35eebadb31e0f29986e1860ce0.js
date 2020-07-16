d6e21d62584bdd646433b6561ad51aa8b2473c8e
its-justus <ianjohnson9042@gmail.com>
2020-07-16 09:28:34 -0500

    cleaning up before styling

---
+++ build/precache-manifest.3575ba35eebadb31e0f29986e1860ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35440e12118682f3011f30eeb7d8c144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f251c75da6e0455094c7",
+    "url": "/static/css/main.ecf8d040.chunk.css"
+  },
+  {
+    "revision": "42272d528ccc0b4c91b1",
+    "url": "/static/js/2.937a968a.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.937a968a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f251c75da6e0455094c7",
+    "url": "/static/js/main.c1df3a4b.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
