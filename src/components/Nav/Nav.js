4b55b4b5dbb5275063597d0eaaa6c478e0d2b2b7
its-justus <ianjohnson9042@gmail.com>
2020-07-16 22:53:12 -0500

    added color palette

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,15 +7,9 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
+      <h2 className="nav-title">TalkAbout</h2>
     </Link>
     <div className="nav-right">
-      <Link className="nav-link" to="/main">
-        {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
-        {!props.user.id && 'Login / Register'}
-      </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
