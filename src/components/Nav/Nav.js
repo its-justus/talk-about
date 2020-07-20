5f35194277c0eaf7e49251ea1bb81102cdbac40e
its-justus <ianjohnson9042@gmail.com>
2020-07-19 19:07:40 -0500

    fixed navbar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,31 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import LogOutButton from "../LogOutButton/LogOutButton";
-import { Typography, Grid, AppBar, Toolbar} from "@material-ui/core";
+import { Typography, Grid, AppBar, Toolbar } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 
-
-const Nav = (props) => { 
+const Nav = (props) => {
   return (
-  <Box bgcolor="secondary.light" height="25px" alignItems="center">
-    <Grid container justify="space-between" alignItems="center">
-      <Grid item xs justify="flex-start">
-        <Typography variant="overline" color="primary">
+    <Box
+			display="flex"
+			flexGrow={1}
+      bgcolor="secondary.light"
+      height="25px"
+      width="100%"
+      alignItems="center"
+    >
+      <Box name="mini-title" display="flex" flexGrow={1} justifySelf="flex-start" paddingX={1}>
+        <Typography
+          variant="subtitle1"
+          style={{ fontFamily: "Fredoka One" }}
+          color="primary"
+        >
           TalkAbout
         </Typography>
-      </Grid>
-      <Grid item xs>
-        <Box display="flex" flexDirection="row-reverse">
-          <LogOutButton className="nav-link" />
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </Box>
-      </Grid>
-    </Grid>
-  </Box>
-);
-}
+      </Box>
+      <Box name="logout" display="flex" justifySelf="flex-end" paddingX={1}>
+        <LogOutButton className="nav-link" />
+      </Box>
+    </Box>
+  );
+};
 
 const mapStateToProps = (state) => ({
   user: state.user,
--- src/style/TalkAboutTheme.js
+++ src/style/TalkAboutTheme.js
@@ -37,7 +37,7 @@ const theme = createMuiTheme({
       paddingLeft: 5,
       lineHeight: 1,
       cursor: "default",
-    },
+		},
   },
 	test: {
 		color: '#ff0000',
